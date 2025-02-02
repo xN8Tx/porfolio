508eaf1160ae89af7e6d91fb51f9a0eed65fcf4c
xN8Tx <theeldern8t@ya.ru>
2025-02-02 17:39:42 +0700

    feat: add ci cd to server

---
+++ config/prod/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git pull origin main
+
+docker compose stop portfolio-client-prod 
+docker compose stop portfolio-strapi-prod 
+
+docker compose rm portfolio-client-prod 
+docker compose rm portfolio-strapi-prod 
+
+docker compose -f ./config/prod/strapi/docker-compose.yaml up --build
+docker compose -f ./config/prod/client/docker-compose.yaml up --build
