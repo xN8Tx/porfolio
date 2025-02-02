ece2b64379fc34cbe67d86cf0db95498956046d7
xN8Tx <theeldern8t@ya.ru>
2025-02-02 18:39:45 +0700

    fix: fix start.sh docker-compose

--- config/prod/start.sh
+++ config/prod/start.sh
@@ -2,11 +2,5 @@
 
 git pull origin main
 
-docker stop portfolio-client-prod 
-docker stop portfolio-strapi-prod 
-
-docker rm portfolio-client-prod 
-docker rm portfolio-strapi-prod 
-
 docker-compose -f ./config/prod/strapi/docker-compose.yaml up --build
 docker-compose -f ./config/prod/client/docker-compose.yaml up --build
