cfd87877333adc11adb35a9ae52dfb74c599a52a
xN8Tx <theeldern8t@ya.ru>
2025-02-02 18:42:14 +0700

    fix: fix start.sh docker-compose

--- config/prod/start.sh
+++ config/prod/start.sh
@@ -2,5 +2,5 @@
 
 git pull origin main
 
-docker-compose -f ./config/prod/strapi/docker-compose.yaml up --build
-docker-compose -f ./config/prod/client/docker-compose.yaml up --build
+docker-compose -f config/prod/strapi/docker-compose.yaml up --build
+docker-compose -f config/prod/client/docker-compose.yaml up --build
