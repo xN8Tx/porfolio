b35c94a69a1cf36fb236b9cba3b7e490ac68f26d
xN8Tx <theeldern8t@ya.ru>
2025-02-04 02:35:17 +0700

    fix: start.sh

--- config/prod/start.sh
+++ config/prod/start.sh
@@ -3,6 +3,6 @@
 docker stop portfolio-strapi-prod portfolio-client-prod
 docker rm portfolio-strapi-prod portfolio-client-prod
 
-docker-compose -f ./strapi/docker-compose.yaml up --build
-docker-compose -f ./client/docker-compose.yaml up --build
+docker-compose -f ./strapi/docker-compose.yaml up --build -d
+docker-compose -f ./client/docker-compose.yaml up --build -d
 
