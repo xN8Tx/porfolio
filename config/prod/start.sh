4af940a59bac27764b7a2661b036df202f5b9075
xN8Tx <theeldern8t@ya.ru>
2025-02-03 21:44:16 +0700

    feat: fix database docker

--- config/prod/start.sh
+++ config/prod/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker stop portfolio-strapi-prod portfolio-client-prod
+docker rm portfolio-strapi-prod portfolio-client-prod
+
 docker-compose -f ./strapi/docker-compose.yaml up --build
 docker-compose -f ./client/docker-compose.yaml up --build
 
